c83abc7364b26355c5ffb860e0b2cdc5ebcb5297
oscar.sanguino <oscar.sanguino@contractor.vroom.com>
2023-06-13 17:14:33 -0500

    updating model to show correctly the auth required

--- apps/forum/models.py
+++ apps/forum/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
+    need_auth = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.name}"
