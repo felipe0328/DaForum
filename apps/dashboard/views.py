d20741f68aea9e7cdc61ec580f2f7dfe72a35472
oscar.sanguino <oscar.sanguino@contractor.vroom.com>
2023-06-14 19:46:19 -0500

    updating view to show the full information

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,4 +1,19 @@
 from apps.forum.views import CategoryListView
+from django.shortcuts import render
 
-class DashboardView(CategoryListView):
+from apps.forum.models import Category, Thread
+
+class DashboardTemplateView(CategoryListView):
     template_name="dashboard/dashboard.html"
+
+
+def DashboardView(request):
+    categories = Category.objects.all()
+    threads = Thread.objects.all()[:10]
+    
+    context = {
+        "categories": categories,
+        "threads": threads
+    }
+
+    return render(request, "dashboard/dashboard.html", context)
