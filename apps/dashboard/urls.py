af9e20d17903594fcc9b2598b900bed21a0c7076
Felipe Puentes <feliipepuentes@gmail.com>
2023-06-15 21:14:19 -0500

    creating signup page

--- apps/dashboard/urls.py
+++ apps/dashboard/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.DashboardView, name='dashboard'),
     path('login', views.LoginInterfaceView.as_view(), name="login"),
     path('logout', views.LogoutInterfaceView.as_view(), name="logout"),
+    path('signup', views.SignupView.as_view(), name="signup"),
 ]
--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect
+
 
 from apps.forum.models import Category, Thread
 
@@ -19,3 +23,14 @@ class LoginInterfaceView(LoginView):
 
 class LogoutInterfaceView(LogoutView):
     template_name='dashboard/logout.html'
+
+class SignupView(CreateView):
+    form_class=UserCreationForm
+    template_name = 'dashboard/register.html'
+    success_url= '/'
+
+    def get(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('/')
+
+        return super().get(request, *args, **kwargs)
